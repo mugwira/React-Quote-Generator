4e000e45db5cf547fbb6f94a2dd34ccbb1ec310e
Mugwira <mugwiram@gmail.com>
2021-09-06 13:02:17 +0200

    Modified Nav component to be responsive

--- src/Nav.js
+++ src/Nav.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 function Nav() {
   return (
     <div className="my-nav text-gray-200 bg-gray-400 ">
-      <div className="my-links flex items-center justify-between py-8 px-32">
-        <div className="right text-2xl font-semi-bold italicnpm install">
+      <div className="my-links md:flex items-center justify-between py-8 px-32">
+        <div className="right text-2xl font-semi-bold italic">
           <h3>Claire</h3>
         </div>
-        <div className="left space-x-16 text-lg">
+        <div className="flex flex-col text-center my-6 space-y-4 md:flex-row items-center space-x-16 text-lg">
           <Link to="/" className="hover:text-gray-300">
             Products
           </Link>
