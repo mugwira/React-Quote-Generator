32f5f49b61a366f6cdec881593ea901d7dcc6e2e
Mugwira <mugwiram@gmail.com>
2021-09-07 22:08:37 +0200

    Added dropdown menu to nav component

--- src/Nav.js
+++ src/Nav.js
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div className="my-nav text-blue-800 bg-gray-300 ">
+    <div className="my-nav text-blue-800 bg-gray-300 py-5 px-5 md:px-24 ">
       <div className="my-links md:flex items-center justify-between">
-        <div className="right text-2xl font-semi-bold italic">
+        <div className="right text-2xl font-bold italic">
           <h3>Claire</h3>
         </div>
-        <div className="left flex flex-col md:flex-row items-center text-lg space-x-16">
-          <Link to="/about" className="transition duration-200 hover:bg-blue-300 px-2">
+        <div className=" flex flex-col py-2 md:flex-row md:space-x-8 md:text-xl">
+          <Link to="/about" className="transition duration-200 hover:text-blue-600 py-2">
             About 
           </Link>
-          <Link to="/Todo" className="hover:text-blue-600">
+          <Link to="/Todo" className="hover:text-blue-600 py-2">
             Todo
           </Link>
-          <Link to="/Quotes" className="hover:text-blue-600">
+          <Link to="/Quotes" className="hover:text-blue-600 py-2">
             Quotes
           </Link>
-          <Link to="/" className="hover:text-blue-600">
+          <Link to="/" className="hover:text-blue-600 py-2">
             Products
           </Link>
         </div>
