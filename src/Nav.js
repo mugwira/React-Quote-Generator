c5dc4b684b6f9b2f9eee4f29f194633e898d1b4a
Mugwira <mugwiram@gmail.com>
2021-09-07 14:14:29 +0200

    Modified Nav and css files

--- src/Nav.js
+++ src/Nav.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <div className="my-nav text-blue-800 bg-gray-300 ">
-      <div className="my-links md:flex items-center justify-between py-4 px-32">
+      <div className="my-links md:flex items-center justify-between">
         <div className="right text-2xl font-semi-bold italic">
           <h3>Claire</h3>
         </div>
-        <div className="flex flex-col md:flex-row items-center text-lg space-x-16">
-          <Link to="/about" className="hover:text-blue-600">
+        <div className="left flex flex-col md:flex-row items-center text-lg space-x-16">
+          <Link to="/about" className="transition duration-200 hover:bg-blue-300 px-2">
             About 
           </Link>
           <Link to="/Todo" className="hover:text-blue-600">
